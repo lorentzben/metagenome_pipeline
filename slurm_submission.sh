2531d81dc58639f061be496998a0e901edc290c1
Benjamin Lorentz <bjl34716@uga.edu>
2021-06-09 08:56:40 -0400

    added slurm script

---
+++ slurm_submission.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=batch
+#SBATCH --job-name=Metagenomic_Pipeline
+#SBATCH --nodes=4
+#SBATCH --ntasks=16
+#SBATCH --time=24:00:00
+#SBATCH --mem=64gb
+
+#Replace this with your UGA email to get notified on completion
+#SBATCH --mail-user="bjl34716@uga.edu"
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load Nextflow/20.04.1
+
+nextflow run main.nf --input seqs --mapping mapping.tsv --outdir paired -resume
